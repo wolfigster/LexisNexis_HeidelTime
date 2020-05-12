4ae206a4aeb74cc1c64a32f3e100005e49815da2
Johannes <Jeis2519@gmail.com>
2020-05-12 16:13:53 +0200

    added file-methods for File.java support

--- src/main/java/de/wolfig/files/Writer.java
+++ src/main/java/de/wolfig/files/Writer.java
@@ -13,52 +13,63 @@ public class Writer {
     private final String CSV_SEPARATOR = ";";
     private final String CSV_LINEBREAK = "\n";
     private BufferedWriter bufferedWriter;
-    private String filePath;
+    private File file;
     private boolean append;
 
-    public Writer(String filePath, boolean append) {
-        this.filePath = filePath;
+    public Writer(File file, boolean append) {
+        this.file = file;
         checkForFile();
         this.append = append;
         try {
-            this.bufferedWriter = new BufferedWriter(new FileWriter(filePath, append));
+            this.bufferedWriter = new BufferedWriter(new FileWriter(file, append));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Writer(String filePath, boolean append) {
+        this(new File(filePath), append);
+    }
+
+    public void changeWriterSettings(File file, boolean append) {
+        this.file = file;
+        checkForFile();
+        this.append = append;
+        try {
+            this.bufferedWriter = new BufferedWriter(new FileWriter(file, append));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public void changeWriterSettings(String filePath, boolean append) {
-        this.filePath = filePath;
+        this.changeWriterSettings(new File(filePath), append);
+    }
+
+    public void changeWriterFile(File file) {
+        this.file = file;
         checkForFile();
-        this.append = append;
         try {
-            this.bufferedWriter = new BufferedWriter(new FileWriter(filePath, append));
+            this.bufferedWriter = new BufferedWriter(new FileWriter(file, append));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public void changeWriterFile(String filePath) {
-        this.filePath = filePath;
-        checkForFile();
-        try {
-            this.bufferedWriter = new BufferedWriter(new FileWriter(filePath, append));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        this.changeWriterFile(new File(filePath));
     }
 
     public void changeWriterAppend(boolean append) {
         this.append = append;
         try {
-            this.bufferedWriter = new BufferedWriter(new FileWriter(filePath, append));
+            this.bufferedWriter = new BufferedWriter(new FileWriter(file, append));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     private void checkForFile() {
-        File file = new File(filePath);
         if(file.exists()) {
             try {
                 file.createNewFile();
