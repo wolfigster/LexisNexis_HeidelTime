b3bba56650b7fa7eb9cad98c2e2181f2047a0a21
Johannes <Jeis2519@gmail.com>
2020-06-03 21:21:41 +0200

    added Tabs

--- src/main/java/de/wolfig/fx/Window.java
+++ src/main/java/de/wolfig/fx/Window.java
@@ -1,8 +1,10 @@
 package de.wolfig.fx;
 
+import com.jfoenix.controls.*;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -18,13 +20,29 @@ public class Window extends Application {
         menuBox.setBackground(new Background(new BackgroundFill(Color.rgb(255, 0, 0), CornerRadii.EMPTY, Insets.EMPTY)));
         menuBox.getChildren().addAll();
 
-
         // main
+        JFXTabPane tabListPane = new JFXTabPane();
+        Tab overviewTab = new Tab();
+        overviewTab.setText("Overview");
+        Tab listTab = new Tab();
+        listTab.setText("List");
+        Tab xmlTab = new Tab();
+        xmlTab.setText("files/xml");
+        Tab txtTab = new Tab();
+        txtTab.setText("files/txt");
+        Tab htTab = new Tab();
+        htTab.setText("files/ht");
+        Tab csvTab = new Tab();
+        csvTab.setText("files/csv");
+        tabListPane.getTabs().addAll(overviewTab, listTab, xmlTab, txtTab, htTab, csvTab);
+
+        // main-list
         VBox listBox = new VBox();
         listBox.setPrefSize(600, 550);
         listBox.setBackground(new Background(new BackgroundFill(Color.rgb(0, 255, 0), CornerRadii.EMPTY, Insets.EMPTY)));
-        listBox.getChildren().addAll();
+        listBox.getChildren().addAll(tabListPane);
 
+        // main-detail
         VBox infoBox = new VBox();
         infoBox.setPrefSize(600, 50);
         infoBox.setBackground(new Background(new BackgroundFill(Color.rgb(0, 0, 255), CornerRadii.EMPTY, Insets.EMPTY)));
@@ -35,11 +53,11 @@ public class Window extends Application {
         fileBox.setBackground(new Background(new BackgroundFill(Color.rgb(255, 255, 0), CornerRadii.EMPTY, Insets.EMPTY)));
         fileBox.getChildren().addAll();
 
-
         VBox detailBox = new VBox();
         detailBox.setPrefSize(600, 550);
         detailBox.getChildren().addAll(infoBox, fileBox);
 
+        // main-main
         HBox mainBox = new HBox();
         mainBox.setPrefSize(1200, 550);
         mainBox.getChildren().addAll(listBox, detailBox);
